eeb9c5ad80a9db3412ba88cd5a8932aae8b61ae7
Mateus Abade <mateusoliver.bsi@gmail.com>
2023-01-22 12:19:03 -0300

    Crud utilizando Scaffolding

---
+++ VendasWebCore/Data/VendasWebCoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VendasWebCore.Models;
+
+namespace VendasWebCore.Data
+{
+    public class VendasWebCoreContext : DbContext
+    {
+        public VendasWebCoreContext (DbContextOptions<VendasWebCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<VendasWebCore.Models.Departamento> Departamento { get; set; } = default!;
+    }
+}
--- VendasWebCore/Program.cs
+++ VendasWebCore/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using VendasWebCore.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<VendasWebCoreContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("VendasWebCoreContext") ?? throw new InvalidOperationException("Connection string 'VendasWebCoreContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
