76736e24655e902a7f385287241ea25fee31278c
Mateus Abade <mateusoliver.bsi@gmail.com>
2023-01-25 19:13:38 -0300

    Serviços

---
+++ VendasWebCore/Data/SeedingService.cs
@@ -0,0 +1,31 @@
+﻿using VendasWebCore.Models;
+using VendasWebCore.Models.Enums;
+
+namespace VendasWebCore.Data
+{
+    public class SeedingService
+    {
+
+
+        public static void Seed(VendasWebCoreContext context)
+        {
+            if(context.Departamento.Any() || context.Vendas.Any() || context.Vendedor.Any() ) 
+            {
+                return;
+            }
+
+            Departamento d1 = new Departamento(1, "Financeiro");
+            Departamento d2 = new Departamento(2, "Faturamento");
+
+            Vendedor ven1 = new Vendedor(1, "Mateus", "exemplo@exemplo.com", 2.200, new DateTime(1991, 08, 17), d1);
+            Vendas v1 = new Vendas(1, new DateTime(), 2.000, StatusVenda.Faturado, ven1);
+
+            context.Departamento.AddRange(d1, d2);
+            context.Vendedor.AddRange(ven1);
+            context.Vendas.AddRange(v1);
+
+            context.SaveChanges();
+        }
+
+    }
+}
--- VendasWebCore/Program.cs
+++ VendasWebCore/Program.cs
@@ -1,15 +1,22 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using VendasWebCore.Data;
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("VendasWebCoreContext");
 builder.Services.AddDbContext<VendasWebCoreContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
+builder.Services.AddScoped<SeedingService>();
+
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+Configure(app);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -31,3 +38,21 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+void Configure(WebApplication host)
+{
+    using var scope = host.Services.CreateScope();
+    var services = scope.ServiceProvider;
+
+    try
+    {
+        var dbContext = services.GetRequiredService<VendasWebCoreContext>();
+        SeedingService.Seed(dbContext);
+
+    }
+    catch (Exception ex)
+    {
+        //Log some error
+        throw;
+    }
+}
