75befe6ebc5e756483f0362eeee173193d103df4
Mateus Abade <mateusoliver.bsi@gmail.com>
2023-02-03 19:43:43 -0300

    criando paginas de busca

---
+++ VendasWebCore/Controllers/VendasController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VendasWebCore.Controllers
+{
+    public class VendasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult BuscaSimples()
+        {
+            return View();
+        }
+
+        public IActionResult BuscaAgrupada()
+        {
+            return View();
+        }
+    }
+}
